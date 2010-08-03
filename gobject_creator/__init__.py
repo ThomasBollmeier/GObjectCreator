a0c68ed0d2588998329e75d0737d25062272618d
Thomas Bollmeier <bolle@t42.(none)>
2010-08-03 16:14:40 +0200

    Changed version info

--- gobject_creator/__init__.py
+++ gobject_creator/__init__.py
@@ -1,9 +1,14 @@
 import os
 import sys
+from datetime import datetime
 
 _root_dir = os.path.dirname(__file__)
 if _root_dir not in sys.path:
     sys.path = [_root_dir] + sys.path
 
-VERSION="0.9.25"
+tstmp = datetime.now()
+
+VERSION="0.9.25-%02d%02d%04d%02d%02d%02d" \
+        (tstmp.day, tstmp.month, tstmp.year,
+         tstmp.hour, tstmp.minute, tstmp.second)
 
